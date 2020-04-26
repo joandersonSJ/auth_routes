e2c724a32e5a8bf7832cbe8a3dff9211504663d4
JoandersonSJ <joandersonsantos376@gmail.com>
2020-04-26 01:22:49 -0300

    completed JavaScript configuration and TypeScript folder was created

--- javascript-expo/src/contexts/auth.js
+++ javascript-expo/src/contexts/auth.js
@@ -1,22 +1,52 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import * as auth from "../sevices/auth"; // Se eu não usar o "* as" da erro
+import { AsyncStorage } from "react-native";
 
 const AuthContext = createContext({ signed: true, user: {} });
 
-export function AuthProvider({ children }) {
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const [ user, setUser ] = useState(null)
+  //Verificação se já existe os itens no storage
+  useEffect(() => {
+    async function loadStorageData() {
+      const storageUser = await AsyncStorage.getItem("@RNAuth:user");
+      const storageToken = await AsyncStorage.getItem("@RNAuth:token");
+
+      if (storageUser && storageToken) {
+
+        // Vai passar um header chamado Authorization com o conteudo definido
+        // api.defaults.headers['Authorization'] = `Bearer ${storageToken}` 
+
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+
+        //Para não ficar um load infinito
+      } else if (!storageUser && !storageToken) {
+        setLoading(false);
+      }
+    }
+    loadStorageData();
+  }, []);
 
   async function signIn() {
     const response = await auth.signIn();
-    setUser(response.user)
+    setUser(response.user);
+
+    // Vai passar um header chamado Authorization com o conteudo definido
+    // api.defaults.headers['Authorization'] = `Bearer ${response.token}`
+
+    await AsyncStorage.setItem("@RNAuth:user", JSON.stringify(response.user));
+    await AsyncStorage.setItem("@RNAuth:token", response.token);
   }
 
   function signOut() {
-    setUser(null)
+    AsyncStorage.clear().then(() => setUser(null));
   }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signIn, signOut}}>
+    <AuthContext.Provider value={{ signed: !!user, user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
